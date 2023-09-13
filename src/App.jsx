3e88064f363e823faaa2a333d09b1558ad3fa43c
stephen nene <stevekid705@gmail.com>
2023-09-13 08:38:11 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import { message } from "antd";
 
 function App() {
   const [scrapeMSG, setscrapeMSG] = useState("");
-  const [greetMsg, setGreetMsg] = useState("");
   const [link, setLink] = useState("");
   const [image, setImage] = useState("https://placehold.co/600x400.png");
   const [error, setError] = useState("");
-  const [name, setName] = useState("");
+
   const [loading, setLoading] = useState(false);
 
 
@@ -24,77 +22,40 @@ function App() {
     }
   }
 
-  async function greet() {
-    if (linkChecker(name, setError)) {
-      // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-      setGreetMsg(await invoke("greet", { name }));
-    }
-  }
 
-  // const scraped = async () => {
-  //   setLoading(true);
-  //   console.log("Loading set to true"); // Add this log statement
-  //   try {
-  //     message.success("here");
-  //     if (linkChecker(link, setError)) {
-  //       const [scrapeMessage, imagePath] = await invoke("capture_screenshot", { link });
-  //       setImage(imagePath); // Set the image path in your state
-  //       message.success({ content: "Scrapped " + link, duration: 2 }); // Display a success message
-  //     }
-  //   } catch (e) {
-  //     console.log('Error: ', e);
-  //     message.error({ content: 'An error occurred at the server', duration: 3 }); // Display an error message
-  //   } finally {
-  //     setLoading(false);
-  //     console.log("Loading set to false"); // Add this log statement
-  //   }
-  // };
+
+
 
   const scrape = async () => {
-    setLoading(true);
-    console.log("Loading set to true"); // Add this log statement
-    try {
-      message.success("here");
+    if (linkChecker(link, setError)) {
+      setLoading(true);
+      try {
+        // const loadingMessage = message.loading({ content: 'Scraping site...', duration: 0 });
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate a delay of 1 second
+        const [scrapeMessage, imagePath] = await invoke("capture_screenshot", { link });
 
-    } catch (e) {
-      console.log('Error: ', e);
-      message.error({ content: 'An error occurred at the server', duration: 3 }); // Display an error message
-    } finally {
-      setLoading(false);
-      console.log("Loading set to false"); // Add this log statement
+        setImage(imagePath); // Set the image path in your state
+
+        message.success({ content: "Scrapped " + link, duration: 2 });
+
+        // loadingMessage(); // Close the loading message
+        setLoading(false);
+
+        console.log('Loading finished'); // Log loading finish
+      } catch (e) {
+        setLoading(false); 
+        console.log('Error: ', e);
+        message.error(e)
+        // message.error({ content: 'An error occurred at the server', duration: 3 }); // Display an error message
+      }
     }
   };
-
-
-
-
 
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-6">Welcome to Steves-bet bot!</h1>
-      {/* <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-        className="flex mt-3 items-center space-x-2"
-      >
-        <input
-          id="greet-input"
-          className={`${
-            error ? "border-red " : "border-gray-300"
-          } border p-2 rounded-lg`}
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-        >
-          Welcome
-        </button>
-      </form> */}
+
 
       <form
         onSubmit={(e) => {
@@ -126,7 +87,6 @@ function App() {
 
 
       {error && <p className="text-red mt-2">{error}</p>}
-      <p className="mt-4">{greetMsg}</p>
       <p className="mt-4">{scrapeMSG}</p>
 
       <img className="my-4" src={image} alt="" />
